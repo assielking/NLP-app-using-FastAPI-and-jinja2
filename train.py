4fe0f7ec9361e2307f5aeaac212d84e433fc1530
unknown <juniorgbodjo@gmail.com>
2024-04-29 23:14:34 +0000

    first commit

---
+++ train.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, Request, Form
+import pickle
+import nltk
+nltk.download('stopwords')
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import uvicorn
+
+with open('pipe.pkl', 'rb') as f:
+    model = pickle.load(f)
+with open('encoder.pkl', 'rb') as f:
+    encoder = pickle.load(f)
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post('/predict')
+async def predict(request: Request, text: str = Form(...)):
+    pred = model.predict([text])
+    prediction = encoder.inverse_transform(pred)[0]
+    return templates.TemplateResponse("index.html", {"request": request, 'data': prediction})
+
+if __name__ == '__main__':
+    uvicorn.run(app)
